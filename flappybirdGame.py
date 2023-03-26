448658f89357b993f8fa84e3801d0f29cd72548a
ninyx <theebatican@gmail.com>
2023-03-27 00:57:22 +0800

    Create class for Flappy Bird game

--- flappybirdGame.py
+++ flappybirdGame.py
@@ -1,2 +1,88 @@
 #Creating Flappy Bird Game with Pygame
 
+#importing modules
+import pygame
+import neat
+import time
+import os
+import random
+
+#Creating window
+WIN_WIDTH = 500
+WIN_HEIGHT = 600
+
+#Loading images
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
+PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
+BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
+BG_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+
+#Creating class for bird
+class FlappyBird:
+    IMAGES = BIRD_IMAGES
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        #position of bird
+        self.x = x
+        self.y = y
+        #bird tilt
+        self.tilt = 0
+        
+        self.jump_counter = 0 #bird jump count
+        self.velocity = 0 #bird velocity
+
+        #bird height
+        self.height = self.y
+
+        #bird image index
+        self.img_count = 0
+        self.img = self.IMAGES[0] 
+
+    def jump(self):
+        self.velocity = -10.5
+        self.jump_counter = 0
+        self.height = self.y
+
+    def move(self):
+        self.jump_counter += 1
+
+        displacement = self.velocity*self.jump_counter + 1.5*self.jump_counter**2 #displacement equation
+
+        if displacement >= 16:
+            displacement = 16
+        if displacement < 0:
+            displacement -= 2
+
+        self.y += displacement
+
+        if displacement < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+    
+    def draw(self, window):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMAGES[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMAGES[2]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMAGES[0]
+            self.img_count = 0
+        
+        if self.tilt <= -80:
+            self.img = self.IMAGES[1]
+            self.img_count = self.ANIMATION_TIME*2
+         
+
+
+while True:
+    FlappyBird.move()
